6d949e9c0c256a05ff96884a44502d46d79cfa30
Pallavi1105 <94678026+Pallavi1105@users.noreply.github.com>
2022-02-04 09:57:44 +0000

    p7

--- p7final.c
+++ p7final.c
@@ -3,27 +3,28 @@ struct _complex
 {
   float real,imaginary;
 };
-typedef _complex complex;
+typedef struct _complex Complex;
 Complex input_complex()
 {
-  complex x;
+  Complex x;
   printf("enter the values\n");
   scanf("%f%f\n",&x.real,&x.imaginary);
   return x;
 }
 Complex add(Complex a, Complex b)
 {
-  complex c;
+  Complex c;
   c.real = a.real + b.real;
   c.imaginary = a.imaginary + b.imaginary;
   return c;
 }
 void output(Complex a, Complex b, Complex c)
 {
-  printf("%f + %fi + %f + %fi is %f + %fi",a.real,a.imaginary,b.real,b.imaginary);
+  printf("%f + %fi + %f + %fi is %f + %fi",a.real,a.imaginary,b.real,b.imaginary,c.real,c.imaginary);
 }
 int main()
 {
+  Complex a,b,c;
   a=input_complex();
   b=input_complex();
   c=add(a,b);
